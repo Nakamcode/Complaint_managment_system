e5cedd3e591edfb8681e72a45b90b93f195d93fe
Nana Asamoah Kwaw <nanaasamoahkwaw963@gmail.com>
2022-03-22 01:29:29 +0000

    php issue solved

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conn = mysqli_connect("localhost","id1382631_cmsnitc17","rootroot","id1382631_cmsnitc17") or die("data base not connected");
+	$conn = mysqli_connect("localhost","root","","complaint system management") or die("data base not connected");
 
 
 ?>
